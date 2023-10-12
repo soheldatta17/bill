3347c153bfc4102bed08603a4f400c1e2021b01f
soheldatta17 <soheldatta17@gmail.com>
2023-10-12 16:47:30 +0530

    Update confirmation.js

--- views/confirmation.js
+++ views/confirmation.js
@@ -6,7 +6,35 @@ window.addEventListener('load', function() {
     {
         this.alert("Response has been successfully submitted")
     }
- 
+
     // You can now use the pageTitle in your JavaScript code
  });
+ window.addEventListener('load', function() {
+    var pageTitleElement = document.getElementById('pageTitle');
+    var pageTitle = pageTitleElement.getAttribute('data-title');
+    
+    if (pageTitle == "success") {
+        // Create a new XMLHttpRequest object
+        var xhr = new XMLHttpRequest();
+
+        // Set up a GET request to index.js
+        xhr.open('GET', '../index.js', true);
+
+        // Define what to do when the response is received
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                // The request was successful
+                console.log(xhr.responseText); // You can process the response here
+            } else {
+                console.error('Request failed with status ' + xhr.status);
+            }
+        };
+
+        // Send the request
+        xhr.send();
+    }
+    
+    // You can now use the pageTitle in your JavaScript code
+});
+
  
